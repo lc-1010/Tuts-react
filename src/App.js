bda068302bcbb09a1a78e4c23dee1781cd929ee2
chao <yahoolichao@gmail.com>
2022-10-11 18:15:54 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
+function App() {
   const title = 'welcome to the new blog';
   const likes = 49;
   const link = "www.google.com";
+  // const title = 'welcome to the new blog';
+  // const likes = 49;
+  // const link = "www.google.com";
+  // return (
+  //   <div className="App">
+  //     <Navbar />
+  //     <div className='content'>
+  //       <Home />
+  //       {/* <p>Liked{likes} times</p>
+  //       <p>{Math.random() * 10}</p> */}
+  //       <a href={link}> google</a>
+  //     </div>
+  //   </div>
+  // );
+
   return (
-    <div className="App">
-      <Navbar />
-      <div className='content'>
-        <Home />
-        {/* <p>Liked{likes} times</p>
-        <p>{Math.random() * 10}</p> */}
-        <a href={link}> google</a>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          {/* <p>Liked{likes} times</p>
+         <p>{Math.random() * 10}</p> */}
+          <a href={link}> google</a>
+        </div>
       </div>
-    </div>
+
+    </Router >
+
+
   );
 }
+
+
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,8 +4,7 @@ const Navbar = () => {
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/crate">
-                    New Blog</a>
+                <a href="/crate">  New Blog</a>
             </div>
         </nav>
 
